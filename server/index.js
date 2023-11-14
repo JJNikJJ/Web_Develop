27bc6b9eb46314a3874aa8b8bdf7e3187fcad185
Nikita <nikita-1707@yandex.ru>
2023-11-14 13:13:03 +0300

    Add new file

--- server/index.js
+++ server/index.js
@@ -1,18 +1,23 @@
-// Пишем все через requier можно все прописать через import но для этого нужны дополнительно настройки
-// через type module
+import express from "express";
+import cors from "cors";
+import testRoute from "./routes/test.js";
 
-import express from 'express'
-import dotenv from 'dotenv'
-const app = express()
-const port = 5000
-async function start(){
-    try{
-        app.listen(port, ()=> {
-            console.log(`server starter om port ${port}` || 3000)
-        })
-    } catch(e){
-        console.log(e)
-    }
+const app = express();
+
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", testRoute);
+async function start() {
+  try {
+    app.listen(port, () => {
+      console.log(`server started on port ${port}` || 3000);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-start()
+start();
